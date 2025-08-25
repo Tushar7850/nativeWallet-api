d0d3b1257110acf3660cc4a9504a301834a27850
Tushar7850 <sawanttushar796@gmail.com>
2025-08-26 01:57:53 +0530

    `Refactored server.js: removed unused imports, moved rateLimiter middleware, added newline at end of file, and updated console logs and health endpoint response.`

--- src/server.js
+++ src/server.js
@@ -1,40 +1,37 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 import transactionsRoute from "./routes/transactionsRoute.js";
 import job from "./config/cron.js";
-dotenv.config();
-console.log("NODE_ENV :", process.env.NODE_ENV);
-if (process.env.NODE_ENV === "production") job.start(); // Start the cron job
 
+dotenv.config();
 
 const app = express();
-app.use(cors());
+
+if (process.env.NODE_ENV === "production") job.start();
 
 // middleware
+app.use(rateLimiter);
 app.use(express.json());
 
+// our custom simple middleware
+// app.use((req, res, next) => {
+//   console.log("Hey we hit a req, the method is", req.method);
+//   next();
+// });
 
-app.get('/api/health', (req, res) => {
-  res.status(200).json({ message: "API is running" });
-})
-
-
-app.use(rateLimiter);
 const PORT = process.env.PORT || 5001;
 
-// middleware to handle JSON requests
-
-
-
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
 
 app.use("/api/transactions", transactionsRoute);
 
 initDB().then(() => {
   app.listen(PORT, () => {
-    console.log("server is running on port 5001");
+    console.log("Server is up and running on PORT:", PORT);
   });
 });
